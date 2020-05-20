518ac2c69866200f16cc2356f7126654a5a0e830
Bruno Domingues <bruno_diego5@hotmail.com>
2020-05-20 01:23:49 -0300

    new alters

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import GlobalStyle from "../styles/global";
 
 // import { Container } from './styles';
 
 function Index() {
   return (
     <>
-      <GlobalStyle />
       <Layout title="Home">
         <p>Welcome to the page</p>
       </Layout>
